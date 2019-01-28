198f69c5363e700b71c90e399e5e1d4950600b9d
sdim-ua <ingersol@yandex.ua>
2019-01-28 21:19:06 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,28 @@
+/* 
+Геттеры и сеттеры для класса Dog
+*/
+
+public class Dog {
+    //напишите тут ваш код
+    String name;
+    int age;
+    
+    public String getName(){
+        return name;
+    }
+    public int age(){
+        return age;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
